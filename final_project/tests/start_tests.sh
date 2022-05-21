d105c2641c94dafaf4c352c0c70220bbf0df19a7
Timofey <nikiforov.ta@edu.spbstu.ru>
2022-05-21 12:50:26 +0300

    update files

--- final_project/tests/start_tests.sh
+++ final_project/tests/start_tests.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-
-cd /tests
-pytest -s -l -v -n "${THREADS:-2}" --alluredir /tmp/allure --selenoid ./ui_tests/
-pytest -s -l -v -n "${THREADS:-2}" --alluredir /tmp/allure ./api_tests/
+cd /tests; pytest -s -l -v -n "${THREADS:-2}" --alluredir /tmp/allure --selenoid ./ui_tests/; pytest -s -l -v -n "${THREADS:-2}" --alluredir /tmp/allure ./api_tests/;
