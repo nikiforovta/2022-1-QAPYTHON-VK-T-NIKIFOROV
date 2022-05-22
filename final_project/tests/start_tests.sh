afcb703de00570e73f69760f5e7d03154b8b8b5f
Timofey <nikiforov.ta@edu.spbstu.ru>
2022-05-22 14:22:14 +0300

    update Jenkinsfile

--- final_project/tests/start_tests.sh
+++ final_project/tests/start_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-cd /tests/utils/db
-python setup.py
+cd /tests/utils/db;
+python setup.py;
 
-cd /tests
-pytest -s -l -v -n "${THREADS:-2}" --alluredir /tmp/allure --selenoid ./ui_tests/
-pytest -s -l -v -n "${THREADS:-2}" --alluredir /tmp/allure ./api_tests/
+cd /tests;
+pytest -s -l -v -n "${THREADS:-2}" --alluredir /tmp/allure --selenoid ./ui_tests/;
+pytest -s -l -v -n "${THREADS:-2}" --alluredir /tmp/allure ./api_tests/;
